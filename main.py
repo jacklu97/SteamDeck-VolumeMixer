9aede91e482c2b66b319eaf242adf82bbec2ccc0
Luis Hernández <jacklu.hdz@gmail.com>
2023-03-28 00:29:48 -0600

    Working version of backend endpoint that gets all processes using sound interface

--- main.py
+++ main.py
@@ -1,15 +1,65 @@
+# Special thanks to jurassicplayer.
+
+import asyncio
+import logging
 import os
+import subprocess
+from typing import List
+
+from settings import SettingsManager # type: ignore
+from helpers import get_user_id # type: ignore
 
 # The decky plugin module is located at decky-loader/plugin
 # For easy intellisense checkout the decky-loader code one directory up
 # or add the `decky-loader/plugin` path to `python.analysis.extraPaths` in `.vscode/settings.json`
 import decky_plugin
 
+# Setup environment variables
+deckyUserHome = os.environ["DECKY_USER_HOME"]
+deckyHomeDir = os.environ["DECKY_HOME"]
+settingsDir = os.environ["DECKY_PLUGIN_SETTINGS_DIR"]
+loggingDir = os.environ["DECKY_PLUGIN_LOG_DIR"]
+defaultAppDataDirectory = os.path.join(deckyUserHome, '.var', 'app')
+XDG_RUNTIME_DIR = os.path.join(os.path.abspath(os.sep), 'run', 'user', str(get_user_id()))
+
+# Setup backend logger
+logging.basicConfig(filename=os.path.join(loggingDir, 'backend.log'),
+                    format='[MusicMixer] %(asctime)s %(levelname)s %(message)s',
+                    filemode='w+',
+                    force=True)
+logger=logging.getLogger()
+logger.setLevel(logging.INFO) # can be changed to logging.DEBUG for debugging issues
+
 
 class Plugin:
-    # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
-    async def add(self, left, right):
-        return left + right
+    async def pyexec_subprocess(self, cmd:str, input:str=''):
+        logging.info(f'Calling python subprocess: "{cmd}"')
+        runtimeDir = os.environ.get("XDG_RUNTIME_DIR")
+        if not runtimeDir: runtimeDir = XDG_RUNTIME_DIR
+        proc = await asyncio.create_subprocess_shell(cmd,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+            stdin=asyncio.subprocess.PIPE,
+            env={"XDG_RUNTIME_DIR": runtimeDir})
+        stdout, stderr = await proc.communicate(input.encode())
+        stdout = stdout.decode()
+        stderr = stderr.decode()
+        logging.info(f'Returncode: {proc.returncode}\nSTDOUT: {stdout[:300]}\nSTDERR: {stderr[:300]}')
+        return {'returncode': proc.returncode, 'stdout': stdout, 'stderr': stderr}
+    
+    async def mm_get_programs_names(self):
+        logger.info('Getting a list of all running processes using audio interface')
+        cmd = "pactl list sink-inputs"
+        proc = await self.pyexec_subprocess(self, cmd)
+
+        if (proc['returncode'] != 0):
+            logging.info("There was an error on the excecution!")
+            return []
+        
+        inputs = [line.strip() for line in proc['stdout'].splitlines() if 'application.name' in line]
+        process_names = [input.split('"')[1] for input in inputs]
+
+        return process_names
 
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
--- test.py
+++ test.py
@@ -1,55 +1,80 @@
 import subprocess
+import os
 
-# Ejecutar el comando pactl list sink-inputs y capturar la salida
-output = subprocess.check_output(["pactl", "list", "sink-inputs"]).decode()
+# # Ejecutar el comando pactl list sink-inputs y capturar la salida
+# output = subprocess.check_output(["pactl", "list", "sink-inputs"]).decode()
 
+# # Buscar las líneas que contienen la información de la aplicación
+# app_lines = [line.strip() for line in output.splitlines() if "application.name" in line]
+
+# # Imprimir el nombre de la aplicación para cada línea encontrada
+# app_names = []
+# for app_line in app_lines:
+#     app_name = app_line.split("=")[1].strip()
+#     print(app_name)
+#     # Los nombres se encontrarán en minusculas, es mejor manejarlos así
+#     app_names.append(str(app_name).lower().strip())
+
+
+
+# Prueba del comando pactl usando otro método
+# output = subprocess.Popen(f"pactl list sink-inputs", stdout=subprocess.PIPE, shell=True, env=self._get_dbus_env(self), universal_newlines=True).communicate()[0])
+def _get_dbus_env():
+    env = os.environ.copy()
+    env["DBUS_SESSION_BUS_ADDRESS"] = f'unix:path=/run/user/{os.getuid()}/bus'
+    return env
+
+
+output = subprocess.Popen(f"pactl list sink-inputs", stdout=subprocess.PIPE, shell=True, env=_get_dbus_env(), universal_newlines=True).communicate()[0]
+print(output)
 # Buscar las líneas que contienen la información de la aplicación
-app_lines = [line.strip() for line in output.splitlines() if "application.name" in line]
+# app_lines = [line.strip() for line in output.splitlines() if "application.name" in line]
 
-# Imprimir el nombre de la aplicación para cada línea encontrada
-app_names = []
-for app_line in app_lines:
-    app_name = app_line.split("=")[1].strip()
-    print(app_name)
-    # Los nombres se encontrarán en minusculas, es mejor manejarlos así
-    app_names.append(str(app_name).lower().strip())
+# # Imprimir el nombre de la aplicación para cada línea encontrada
+# app_names = []
+# for app_line in app_lines:
+#     app_name = app_line.split("=")[1].strip()
+#     print(app_name)
+#     # Los nombres se encontrarán en minusculas, es mejor manejarlos así
+#     app_names.append(str(app_name).lower().strip())
 
 
-# Página de la librería
-import pulsectl
 
-# Conectar al servidor de sonido PulseAudio
-pulse = pulsectl.Pulse('my-client-name')
+# # Página de la librería
+# import pulsectl
 
-# Obtener el objeto de control de volumen para la aplicación
-sink_input_list = pulse.sink_input_list()
-for i in sink_input_list:
-    # El listado arroja los nombres usando codificacion por lo que hay que hacer un cast
-    if str(i.proplist.get('application.process.binary')) == 'firefox':
-        sink_input = pulse.sink_input_info(i.index)
-        volume = sink_input.volume
-        break
-    else:
-        print("No es lo mismo")
+# # Conectar al servidor de sonido PulseAudio
+# pulse = pulsectl.Pulse('my-client-name')
 
-# Obtener el volumen actual
-current_volume = volume.value_flat
+# # Obtener el objeto de control de volumen para la aplicación
+# sink_input_list = pulse.sink_input_list()
+# for i in sink_input_list:
+#     # El listado arroja los nombres usando codificacion por lo que hay que hacer un cast
+#     if str(i.proplist.get('application.process.binary')) == 'firefox':
+#         sink_input = pulse.sink_input_info(i.index)
+#         volume = sink_input.volume
+#         break
+#     else:
+#         print("No es lo mismo")
 
-# Establecer el volumen al porcentaje deseado
-new_volume_val = float(1 * 1)
-new_volume = pulsectl.PulseVolumeInfo(new_volume_val, len(volume.values))
-pulse.volume_set(sink_input, new_volume)
+# # Obtener el volumen actual
+# current_volume = volume.value_flat
 
-# SteamOS parece usar pipewire por defecto
-import pwclient
+# # Establecer el volumen al porcentaje deseado
+# new_volume_val = float(1 * 1)
+# new_volume = pulsectl.PulseVolumeInfo(new_volume_val, len(volume.values))
+# pulse.volume_set(sink_input, new_volume)
 
-# Conectar con el daemon de PipeWire
-client = pwclient.Client()
+# # SteamOS parece usar pipewire por defecto
+# import pwclient
 
-# Obtener el volumen actual de la salida predeterminada
-volume_info = client.get_volume('default')
-current_volume = volume_info.values[0]
+# # Conectar con el daemon de PipeWire
+# client = pwclient.Client()
 
-# Establecer un nuevo volumen
-new_volume = current_volume - 0.1  # Reducir el volumen en un 10%
-client.set_volume('default', new_volume)
+# # Obtener el volumen actual de la salida predeterminada
+# volume_info = client.get_volume('default')
+# current_volume = volume_info.values[0]
+
+# # Establecer un nuevo volumen
+# new_volume = current_volume - 0.1  # Reducir el volumen en un 10%
+# client.set_volume('default', new_volume)
