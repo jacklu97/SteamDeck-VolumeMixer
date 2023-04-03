785144d17d0f167cf6800ac844a47eaf979e1e26
Luis Hernández <jacklu.hdz@gmail.com>
2023-04-03 17:24:16 -0600

    Remove unused testing code on index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import {
   definePlugin,
-  DialogButton,
-  Router,
   ServerAPI,
 } from "decky-frontend-lib";
-import { VFC } from "react";
 import { RxMixerVertical } from "react-icons/rx";
 
 import App from "./components/App/App";
@@ -12,31 +9,14 @@ import Title from "./components/UI/Title";
 
 import PythonServer from "./services/pythonServer";
 
-
-const DeckyPluginRouterTest: VFC = () => {
-  return (
-    <div style={{ marginTop: "50px", color: "white" }}>
-      Hello World!
-      <DialogButton onClick={() => Router.NavigateToLibraryTab()}>
-        Go to Library
-      </DialogButton>
-    </div>
-  );
-};
-
 export default definePlugin((serverApi: ServerAPI) => {
   PythonServer.getInstance().setServer(serverApi)
   
-  serverApi.routerHook.addRoute("/decky-plugin-test", DeckyPluginRouterTest, {
-    exact: true,
-  });
-
   return {
     title: <Title />,
     content: <App />,
     icon: <RxMixerVertical />,
     onDismount() {
-      serverApi.routerHook.removeRoute("/decky-plugin-test");
     },
   };
 });
