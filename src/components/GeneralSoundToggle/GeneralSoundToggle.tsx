1fdeee91c015732999011afa158c0fdc9dfe9e7d
Luis Hernández <jacklu.hdz@gmail.com>
2023-04-03 02:21:03 -0600

    Implement usage of toggle mute Implement usage of current mute status to update UI

--- src/components/GeneralSoundToggle/GeneralSoundToggle.tsx
+++ src/components/GeneralSoundToggle/GeneralSoundToggle.tsx
@@ -1,12 +1,38 @@
+import { useEffect, useState } from "react";
+
 import { PanelSection, PanelSectionRow, ToggleField } from "decky-frontend-lib";
 
+import PythonServer from "../../services/pythonServer";
+
 const GeneralSoundToggle = () => {
+  const [isMuted, setIsMuted] = useState<boolean>(false)
+
+  const server = PythonServer.getInstance()
+
+  const handleToggle = () => {
+    server.resolve(server.toggleMuteStatus(), () => setIsMuted(prev => !prev))
+  }
+
+  const getCurrentMuteStatus = (): Promise<MuteStatusResponse> => {
+    return new Promise<MuteStatusResponse>((resolve) => {
+      server.resolve(server.getMuteStatus(), (response: MuteStatusResponse) => {
+        resolve(response)
+      })
+    })
+  }
+
+  useEffect(() => {
+    getCurrentMuteStatus()
+      .then(res => setIsMuted(res.isMuted))
+  }, [])
+
   return (
     <PanelSection title="System Volume">
       <PanelSectionRow>
         <ToggleField
           label="Mute System"
-          checked={false}
+          checked={isMuted}
+          onChange={handleToggle}
         />
       </PanelSectionRow>
     </PanelSection>
