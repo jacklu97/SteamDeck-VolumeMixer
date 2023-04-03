b02de98a5e0bef0ed7962678d4468037f1a590b9
Luis Hernández <jacklu.hdz@gmail.com>
2023-04-02 20:07:37 -0600

    Remove unused content in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,8 @@
 import {
-  ButtonItem,
   definePlugin,
   DialogButton,
-  Menu,
-  MenuItem,
-  PanelSection,
-  PanelSectionRow,
   Router,
   ServerAPI,
-  showContextMenu,
 } from "decky-frontend-lib";
 import { VFC } from "react";
 import { RxMixerVertical } from "react-icons/rx";
@@ -16,70 +10,8 @@ import { RxMixerVertical } from "react-icons/rx";
 import App from "./components/App/App";
 import Title from "./components/UI/Title";
 
-import logo from "../assets/logo.png";
 import PythonServer from "./services/pythonServer";
 
-// interface AddMethodArgs {
-//   left: number;
-//   right: number;
-// }
-
-const Content: VFC<{ serverAPI: ServerAPI }> = ({}) => {
-  // const [result, setResult] = useState<number | undefined>();
-
-  // const onClick = async () => {
-  //   const result = await serverAPI.callPluginMethod<AddMethodArgs, number>(
-  //     "add",
-  //     {
-  //       left: 2,
-  //       right: 2,
-  //     }
-  //   );
-  //   if (result.success) {
-  //     setResult(result.result);
-  //   }
-  // };
-
-  return (
-    <PanelSection title="Panel Section">
-      <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={(e: any) =>
-            showContextMenu(
-              <Menu label="Menu" cancelText="CAAAANCEL" onCancel={() => {}}>
-                <MenuItem onSelected={() => {}}>Item #1</MenuItem>
-                <MenuItem onSelected={() => {}}>Item #2</MenuItem>
-                <MenuItem onSelected={() => {}}>Item #3</MenuItem>
-              </Menu>,
-              e.currentTarget ?? window
-            )
-          }
-        >
-          I'm Changing!
-        </ButtonItem>
-      </PanelSectionRow>
-
-      <PanelSectionRow>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <img src={logo} />
-        </div>
-      </PanelSectionRow>
-
-      <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={() => {
-            Router.CloseSideMenus();
-            Router.Navigate("/decky-plugin-test");
-          }}
-        >
-          Router
-        </ButtonItem>
-      </PanelSectionRow>
-    </PanelSection>
-  );
-};
 
 const DeckyPluginRouterTest: VFC = () => {
   return (
@@ -101,7 +33,6 @@ export default definePlugin((serverApi: ServerAPI) => {
 
   return {
     title: <Title />,
-    // content: <Content serverAPI={serverApi} />,
     content: <App />,
     icon: <RxMixerVertical />,
     onDismount() {
